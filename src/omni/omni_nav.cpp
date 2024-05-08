52e928f17b618fcf2877934dafe5edc1e7d8ed57
moririn777 <moriya8229@gmail.com>
2024-05-08 22:23:00 +0900

    feat:オムニホイールの自己位置推定機能

---
+++ src/omni/omni_nav.cpp
@@ -0,0 +1,5 @@
+#include "omni_nav.h"
+
+OmniNav::OmniNav(int radius) { this->radius = radius; }
+
+void OmniNav::getPositionX() {}
---
+++ src/omni/omni_nav.h
@@ -0,0 +1,14 @@
+#ifndef ROBOKEN_HEADERS_OMNI_NAVICATION_H
+#define ROBOKEN_HEADERS_OMNI_NAVICATION_H
+
+class OmniNav {
+    public:
+        OmniNav(int radius);
+        void getPositionX();
+        void getPositionY();
+
+    private:
+        int radius;
+};
+
+#endif
