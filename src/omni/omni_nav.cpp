8e70f74e8887ed2ff436150e75ccd69f65a36ba9
moririn777 <moriya8229@gmail.com>
2024-05-09 00:05:34 +0900

    feat:モータ回転数を与えられるようにした

--- src/omni/omni_nav.cpp
+++ src/omni/omni_nav.cpp
@@ -1,5 +1,16 @@
 #include "omni_nav.h"
 
-OmniNav::OmniNav(int radius) { this->radius = radius; }
+#include <math.h>
+OmniNav::OmniNav(double radius) { this->radius = radius; }
 
-void OmniNav::getPositionX() {}
+void OmniNav::setRevolution(double v1, double v2, double v3) {
+  this->revolution1 = v1;
+  this->revolution2 = v2;
+  this->revolution3 = v3;
+}
+double OmniNav::getPositionX() {
+  return (1 / 3 * ((2 * revolution1) - revolution2 - revolution3));
+}
+double OmniNav::getPositionY() {
+  return ((sqrt(3) / 3) * (revolution2 - revolution3));
+}
--- src/omni/omni_nav.h
+++ src/omni/omni_nav.h
@@ -3,12 +3,18 @@
 
 class OmniNav {
  public:
-  OmniNav(int radius);
-  void getPositionX();
-  void getPositionY();
+  OmniNav(double radius);
+  void setRevolution(
+      double v1, double v2,
+      double v3);  // v1をX軸に水平,v2,v3をccwの順番のモータの回転数とする。
+  double getPositionX();
+  double getPositionY();
 
  private:
-  int radius;
+  double radius;  // モータの半径
+  double revolution1;
+  double revolution2;
+  double revolution3;
 };
 
 #endif
