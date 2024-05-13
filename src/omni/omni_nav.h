27f87d52bc793e7decd7b5c35cb4e0ec6b834cbb
moririn777 <moriya8229@gmail.com>
2024-05-14 01:19:37 +0900

    refactor:座標を取得する関数名を修正

--- src/omni/omni_nav.h
+++ src/omni/omni_nav.h
@@ -7,16 +7,21 @@ class OmniNav {
   void setRevolution(
       double v1, double v2,
       double v3);  // v1をX軸に水平,v2,v3をccwの順番のモータの回転数とする。
-  double getPositionX();
-  double getPositionY();
-  double getRadian();
+  double getDeltaX();
+  double getDeltaY();
+  double getDeltaRad();
 
  private:
   double radius;  // ロボット中心からのオムニホイールまでの距離
   double circumference;  // オムニホイールの円周の長さ
+
   double revolution1 = 0.0;
   double revolution2 = 0.0;
   double revolution3 = 0.0;
+
+  double deltaX = 0.0;    // xの移動距離
+  double deltaY = 0.0;    // yの移動距離
+  double deltaRad = 0.0;  // 回転角度 [rad]
 };
 
 #endif
